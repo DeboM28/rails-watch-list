62a90ad988b62f65b421bfadc1ca8ca228a43f41
Deborah Parent <deborah.morchipont@hotmail.fr>
2023-11-24 12:33:45 +0100

    nimporte

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+require 'open-uri'
+
+Movie.destroy_all
+
+puts 'start seeding'
+
+url = 'http://tmdb.lewagon.com/movie/top_rated?'
+movies_serialized = URI.open(url).read
+results = JSON.parse(movies_serialized)
+
+movies = results['results']
+
+movies.first(5)
+movies.each do |movie|
+  Movie.create(title: movie['title'], overview: movie['overview'], rating: movie['vote_average'], poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}")
+end
+
+puts 'seeds done'
+
+List.destroy_all
+
+List.create(name:"Liste1")
+List.create(name:"Liste2")
+List.create(name:"Liste3")
+List.create(name:"Liste4")
